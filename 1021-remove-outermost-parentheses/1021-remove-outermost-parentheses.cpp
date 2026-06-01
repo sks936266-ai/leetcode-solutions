68d56938bdba3296b1d42493ca912c6c6c770536
sks936266-ai <sks936266@gmail.com>
2026-06-01 21:05:34 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.48%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result="";
+        int count=0;
+        for(char c:s){
+            if(c=='('){
+                if(count>0){
+                    result+=c;
+                }
+                count++;
+            }
+            else{
+                count--;
+                if(count>0){
+                    result+=c;
+                }
+            }
+        }
+        return result;
+    }
+};
