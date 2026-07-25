739970470ee46ea9ac8e7feb4fefe4f48efbb716
sks936266-ai <sks936266@gmail.com>
2026-07-25 14:22:29 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int j=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                j=i;
-                break;
+        int write=0;
+        for(int r=0 ;r< nums.size();r++){
+            if(nums[r]!=0){
+                nums[write]=nums[r];
+                write++;
             }
         }
-        if(j==-1) return;
-        for(int i=j+1;i<n;i++){
-            if(nums[i]!=0){
-                swap(nums[i],nums[j]);
-                j++;
-            }
+        while(write<nums.size()){
+            nums[write]=0;
+            write++;
         }
     }
 };
