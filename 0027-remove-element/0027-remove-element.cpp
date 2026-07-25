b17d41855aeb381e97fd9cb7c0bd96fe5261aa96
sks936266-ai <sks936266@gmail.com>
2026-07-25 17:16:36 +0530

    Time: 0 ms (100%), Space: 11.6 MB (85.25%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int write=0;
+        for(int read=0;read<nums.size();read++){
+            if(nums[read]!=val){
+                nums[write]=nums[read];
+                write++;
+            }
+        }
+        return write;
+    }
+};
