8228e4a76d3a74bb1a3ddb9ce6a9fc620eb4b49c
sks936266-ai <sks936266@gmail.com>
2026-07-27 17:49:03 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,39 +1,24 @@
-#include <stack>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     bool isValid(string s) {
-        // Base Case: Agar length odd hai, toh kabhi valid nahi ho sakta
-        if (s.length() % 2 != 0) return false;
         
         stack<char> st;
-        
-        for (char c : s) {
-            // 1. Agar opening brackets hain, toh unke CLOSING partners push karo
-            if (c == '(') {
-                st.push(')');
-            } 
-            else if (c == '[') {
-                st.push(']');
-            } 
-            else if (c == '{') {
-                st.push('}');
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(ch=='('|| ch=='[' || ch=='{'){
+                st.push(ch);
             }
-            // 2. Agar closing bracket aaya hai
-            else {
-                // Pehle check karo stack khali toh nahi hai (e.g., s = "}")
-                // Ya fir stack ka top current character se match nahi ho raha (e.g., s = "(]")
-                if (st.empty() || st.top() != c) {
+            else{
+                if(st.empty()){
                     return false;
                 }
-                // Agar match ho gaya, toh chupchaap pop karo aur aage badho
-                st.pop();
+                char top=st.top();
+                if((ch==')'&& top=='(')||(ch==']'&&top=='[')||(ch=='}'&&top=='{')){
+                    st.pop();
+                }
+                else{ return false;}
             }
         }
-        
-        // Aakhiri me agar stack khali hai, toh hi string valid hai
         return st.empty();
     }
 };
