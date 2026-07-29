524c2af42f00da7e75e6c0ab7f0e34977baa3469
sks936266-ai <sks936266@gmail.com>
2026-07-29 23:55:34 +0530

    Time: 0 ms (100%), Space: 14.3 MB (7.08%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n=prices.size();
+        vector<int> answer(n);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()>prices[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                answer[i]=prices[i]-st.top();
+            }
+            else{
+                answer[i]=prices[i];
+            }
+            st.push(prices[i]);
+        }
+        return answer;
+    }
+};
