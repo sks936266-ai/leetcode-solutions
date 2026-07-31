aa9bbc3a120260b162b14a850e4be9d81cd2995c
sks936266-ai <sks936266@gmail.com>
2026-07-31 22:18:44 +0530

    Time: 13 ms (56.97%), Space: 114.4 MB (68.61%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    std::vector<int> mostCompetitive(std::vector<int>& nums, int k) {
-        std::vector<int> stack;
-        int n = nums.size();
-        
-        for (int i = 0; i < n; ++i) {
-            // While stack is not empty, current val is smaller than top,
-            // and we have enough remaining elements to fill the stack to size k
-            while (!stack.empty() && nums[i] < stack.back() && 
-                   stack.size() + (n - i) > k) {
-                stack.pop_back();
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty()&& st.back()>nums[i]&&(st.size()+n-i>k)){
+                st.pop_back();
             }
-            
-            // Only push if we haven't filled the required length k
-            if (stack.size() < k) {
-                stack.push_back(nums[i]);
+            if(st.size()<k){
+                st.push_back(nums[i]);
             }
         }
-        
-        return stack;
+        return st;
     }
-};
+};  
