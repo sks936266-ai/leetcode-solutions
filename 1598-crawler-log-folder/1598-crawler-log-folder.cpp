001c02c6253e9b1e014dfb83d48ece1d37b53935
sks936266-ai <sks936266@gmail.com>
2026-08-05 18:24:38 +0530

    Time: 0 ms (100%), Space: 14.4 MB (26.56%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string>st;
+        for(string &log:logs){
+            if(log=="../"){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else if(log!="./"){
+                st.push(log);
+            }
+            
+        }
+        return st.size();
+    }
+};
