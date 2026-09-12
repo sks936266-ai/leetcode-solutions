8dbf8adcf466bd5a0020518edd264a209c2b4c3b
sks936266-ai <sks936266@gmail.com>
2026-09-13 01:01:58 +0530

    Time: 0 ms (100%), Space: 14.2 MB (53.25%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    int countonebits(int num){
-        int count=0;
-        while(num!=0){
-            count+=(num&1);
-            num>>=1;
-        }
-        return count;
-    }
     vector<int> sortByBits(vector<int>& arr) {
         auto lamda=[this](int &a,int &b){
-            int count_a=countonebits(a);
-            int count_b=countonebits(b);
+            int count_a=__builtin_popcount(a);
+            int count_b=__builtin_popcount(b);
             if(count_a==count_b){
                 return a<b;
             }
-            return count_a < count_b;
+            return count_a<count_b;
         };
         sort(begin(arr),end(arr),lamda);
         return arr;
