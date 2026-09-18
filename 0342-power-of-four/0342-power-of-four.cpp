ae906c505289b77a41a2eb4845200f454d841c57
sks936266-ai <sks936266@gmail.com>
2026-09-19 00:14:35 +0530

    Time: 0 ms (100%), Space: 7.8 MB (48.26%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+
+        if((n&(n-1))==0 && (n-1)%3==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
